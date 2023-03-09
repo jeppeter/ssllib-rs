bc39353443a2d428962afcaa9c7fd4e6f68f1b1a
jeppeter <jeppeter@gmail.com>
2023-03-09 10:27:55 +0800

    to add iniwrite and inidel

--- utest/utest/src/iniexec.rs
+++ utest/utest/src/iniexec.rs
@@ -21,6 +21,7 @@ use std::any::Any;
 use lazy_static::lazy_static;
 use std::collections::HashMap;
 
+#[allow(unused_imports)]
 use super::*;
 use super::loglib::*;
 #[allow(unused_imports)]
@@ -35,15 +36,19 @@ extargs_error_class!{IniExecError}
 
 fn iniread_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl>>>,_ctx :Option<Arc<RefCell<dyn Any>>>) -> Result<(),Box<dyn Error>> {	
 	let sarr :Vec<String>;
+	let mut sout = std::io::stdout();
+	let mut s :String;
 
 	init_log(ns.clone())?;
 	sarr = ns.get_array("subnargs");
 	for f in sarr.iter() {
 		let i = Ini::load_from_file(f)?;
 		for (sec,prop) in i.iter() {
-			debug_trace!("[{}] section [{:?}]", f,sec);
+			s = format!("[{}] section [{:?}]\n", f,sec);
+			sout.write(s.as_bytes())?;
 			for (k,v) in prop.iter() {
-				debug_trace!("[{}]=[{}]",k,v);
+				s = format!("    [{}]=[{}]\n",k,v);
+				sout.write(s.as_bytes())?;
 			}
 		}
 	}
@@ -51,11 +56,79 @@ fn iniread_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl
 	Ok(())
 }
 
-#[extargs_map_function(iniread_handler)]
+fn iniwrite_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl>>>,_ctx :Option<Arc<RefCell<dyn Any>>>) -> Result<(),Box<dyn Error>> {	
+	let sarr :Vec<String>;
+	//let mut sout = std::io::stdout();
+	let mut i :Ini;
+
+	init_log(ns.clone())?;
+	sarr = ns.get_array("subnargs");
+	if sarr.len() < 1 {
+		extargs_new_error!{IniExecError,"need file at least"}
+	}
+
+	i = Ini::load_from_file(&(sarr[0]))?;
+
+	for f in sarr[1..].iter() {
+		let carr :Vec<&str>;
+		carr = f.split("=").collect();
+		if carr.len() > 1 {
+			let barr :Vec<&str>;
+			barr = carr[0].split(".").collect();
+			if barr.len() > 1 {
+				debug_trace!("set [{}].[{}] = [{}]",barr[0],barr[1],carr[1]);
+				i.with_section(Some(barr[0])).set(barr[1],carr[1]);
+			} else {
+				debug_trace!("set [{}] = [{}]",carr[0],carr[1]);
+				i.with_section(None::<String>).set(carr[0],carr[1]);
+			}
+		} 
+	}
+
+	let _ = i.write_to_file(&sarr[0])?;
+
+	Ok(())
+}
+
+fn inidel_handler(ns :NameSpaceEx,_optargset :Option<Arc<RefCell<dyn ArgSetImpl>>>,_ctx :Option<Arc<RefCell<dyn Any>>>) -> Result<(),Box<dyn Error>> {	
+	let sarr :Vec<String>;
+	//let mut sout = std::io::stdout();
+	let mut i :Ini;
+
+	init_log(ns.clone())?;
+	sarr = ns.get_array("subnargs");
+	if sarr.len() < 1 {
+		extargs_new_error!{IniExecError,"need file at least"}
+	}
+
+	i = Ini::load_from_file(&(sarr[0]))?;
+
+	for f in sarr[1..].iter() {
+		let carr :Vec<&str>;
+		carr = f.split(".").collect();
+		if carr.len() > 1 {
+			i.delete_from(Some(carr[0]),carr[1]);
+		} else {
+			i.delete(Some(carr[0]));
+		}
+	}
+
+	let _ = i.write_to_file(&sarr[0])?;
+
+	Ok(())
+}
+
+#[extargs_map_function(iniread_handler,iniwrite_handler,inidel_handler)]
 pub fn load_iniexec_handler(parser :ExtArgsParser) -> Result<(),Box<dyn Error>> {
 	let cmdline = r#"
 	{
-		"iniread<iniread_handler>##file ... to decode x509##" : {
+		"iniread<iniread_handler>##file ... to read ini file##" : {
+			"$" : "+"
+		},
+		"iniwrite<iniwrite_handler>##file sec.key=val ... to set ini value##" : {
+			"$" : "+"
+		},
+		"inidel<inidel_handler>##file sec[.key] ... to delete ini value##" : {
 			"$" : "+"
 		}
 	}
