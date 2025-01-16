c4fdfd4f267345e2cbe3fe8f6bd12aaaa2aca51e
jeppeter <jeppeter@gmail.com>
2025-01-16 15:56:09 +0800

    to add gennames

---
+++ utest/utest/src/spc.rs
@@ -0,0 +1,73 @@
+#[allow(unused_imports)]
+use asn1obj_codegen::{asn1_choice,asn1_obj_selector,asn1_sequence,asn1_int_choice};
+
+
+#[asn1_sequence()]
+#[derive(Clone)]
+pub struct SpcAttributeTypeAndOptionalValueElem {
+	pub itype :Asn1Object,
+	pub value :Asn1Any,
+}
+
+#[asn1_sequence()]
+#[derive(Clone)]
+pub struct SpcAttributeTypeAndOptionalValue {
+	pub elem :Asn1Seq<SpcAttributeTypeAndOptionalValueElem>,
+}
+
+
+#[asn1_sequence()]
+#[derive(Clone)]
+pub struct AlgorithmIdentifierElem {
+	pub algorithm :Asn1Object,
+	pub parameters :Asn1Any,
+}
+
+#[asn1_sequence()]
+#[derive(Clone)]
+pub struct AlgorithmIdentifier {
+	pub elem :Asn1Seq<AlgorithmIdentifierElem>,
+}
+
+
+#[asn1_sequence()]
+#[derive(Clone)]
+pub struct DigestInfoElem {
+	pub digestAlgorithm :AlgorithmIdentifier,
+	pub digest : Asn1OctData,
+}
+
+#[asn1_sequence()]
+#[derive(Clone)]
+pub struct DigestInfo {
+	pub elem :Asn1Seq<DigestInfoElem>,
+}
+
+#[asn1_sequence()]
+#[derive(Clone)]
+pub struct SpcIndirectDataContentElem {
+	pub data :SpcAttributeTypeAndOptionalValue,
+	pub messageDigest :DigestInfo,
+}
+
+#[asn1_sequence()]
+#[derive(Clone)]
+pub struct SpcIndirectDataContent {
+	pub elem :Asn1Seq<SpcIndirectDataContentElem>,
+}
+
+
+pub struct TimeStampTokenElem {
+	pub version :Asn1Integer,
+	pub policy_id :Asn1Object,
+	pub messageImprint :MessageImprint,
+	pub serial :Asn1BigNum,
+	pub time :Asn1Time,
+	pub accuracy :TimeStampAccuracy,
+	pub ordering :Asn1Bool,
+	pub nonce :Asn1Integer,
+}
+
+pub struct TimeStampToken {
+	pub elem :Asn1Seq<TimeStampTokenElem>,
+}
