d2d55c7b6f4d13362ed70f8abbf0fc34edf80310
jeppeter <jeppeter@gmail.com>
2025-08-06 14:56:19 +0800

    to add sub_signed_cert.pem

--- utest/utest/generate_ca_sign.sh
+++ utest/utest/generate_ca_sign.sh
@@ -32,3 +32,22 @@ EOF
 
 
 openssl x509 -req -in my_cert_req.pem -days 365 -CA ca_cert.pem -CAkey ca_private_key.pem -CAcreateserial -out my_signed_cert.pem
+
+openssl genrsa -out sub_private_key.pem 4096
+
+openssl req -new -key sub_private_key.pem -out sub_cert_req.pem -config - << EOF
+[req]
+prompt = no
+utf8 = yes
+string_mask = utf8only
+distinguished_name = dn
+default_days     = 1000  
+
+# this is names
+[dn]
+CN=sub Root CA
+
+EOF
+
+
+openssl x509 -req -in sub_cert_req.pem -days 365 -CA my_signed_cert.pem -CAkey my_private_key.pem -CAcreateserial -out sub_signed_cert.pem
