287f299cbc5077be032873062f6dff8a7793c242
jeppeter <jeppeter@gmail.com>
2025-09-05 13:45:16 +0800

    to make _form_ext_key_usage before _form_constraints_valid

--- src/x509.rs
+++ src/x509.rs
@@ -1523,13 +1523,13 @@ impl Asn1X509CinfElem {
 		retv.key = pubkey.export_pubkey()?;
 		retv._form_cfg_serial_number(cfg)?;
 		retv._form_key_usage(cfg)?;
+		retv._form_ext_key_usage(cfg)?;
 		retv._form_constraints_valid(cfg)?;
 		retv.signature = privkey.export_signature_algo()?;
 		retv._form_issuer_and_subject(cfg)?;
 		retv._form_cfg_time(cfg)?;
 		retv._form_subject_key_id(cfg)?;
 		retv._form_altname(cfg)?;
-		retv._form_ext_key_usage(cfg)?;
 		retv._form_policies(cfg)?;
 		retv._form_authority_key_id(cfg)?;
 		retv._form_ocsp_servers_and_issuer_certificate_urls(cfg)?;
